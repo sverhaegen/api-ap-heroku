10a3b721c960bbd4193b7a262f271a30781d8fda
sverhaegen <56384380+sverhaegen@users.noreply.github.com>
2021-02-24 19:58:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://www.michaelboeynaems.com'
+        origin: 'https://heuristic-curie-65d4b0.netlify.app/'
     })
 );
 app.options('*', cors());
